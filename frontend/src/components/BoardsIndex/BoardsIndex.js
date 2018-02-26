988963c3587aa2c4d889fcbba2da862880c7d2b7
Emilio Cristalli <emilio@wyeworks.com>
2018-02-25 21:19:36 -0300

    Boards index styles

--- frontend/src/components/BoardsIndex/BoardsIndex.js
+++ frontend/src/components/BoardsIndex/BoardsIndex.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './BoardsIndex.css';
 import BoardTile from '../BoardTile/BoardTile';
 
@@ -7,16 +6,20 @@ class BoardsIndex extends Component {
   render() {
     return (
       <div className="BoardsIndex">
-        <header className="BoardsIndex-header">
-          <img src={logo} className="BoardsIndex-logo" alt="logo" />
-          <h1 className="BoardsIndex-title">Welcome to React</h1>
-        </header>
-        <p className="BoardsIndex-intro">
-          To get started, edit <code>src/BoardsIndex.js</code> and save to reload.
-        </p>
-        <BoardTile name="Board-1" />
-        <BoardTile name="Board-2" color="rgb(150, 121, 191)" />
-        <BoardTile name="Board-3" color="rgb(150, 0, 0)" />
+        <div className="BoardsIndex-container">
+          <div className="BoardsIndex-header">
+            <div className="BoardsIndex-header-icon">
+              <span className="BoardsIndex-header-icon-member"></span>
+            </div>
+            <h3 className="BoardsIndex-header-name">Personal Boards</h3>
+          </div>
+
+          <ul className="BoardsIndex-list-container">
+            <BoardTile name="Board-1" />
+            <BoardTile name="Board-2" color="rgb(150, 121, 191)" />
+            <BoardTile name="Board-3" color="rgb(150, 0, 0)" />
+          </ul>
+        </div>
       </div>
     );
   }
