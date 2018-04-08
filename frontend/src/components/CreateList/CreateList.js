b885683a332df4a27b72ca875f822cf7a10ec244
Emilio Cristalli <emilio@wyeworks.com>
2018-04-08 18:14:21 -0300

    Add list functionality

--- frontend/src/components/CreateList/CreateList.js
+++ frontend/src/components/CreateList/CreateList.js
@@ -2,10 +2,32 @@ import React, { Component } from 'react';
 import './CreateList.css';
 
 class CreateList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { addingList: false };
+  }
+
+  toggleAddList = () => {
+    this.setState(prevState => ({
+      addingList: !prevState.addingList
+    }));
+  }
+
   render() {
     return (
-      <div className="CreateList">
-        <span className="CreateList-placeholder">Add a list…</span>
+      <div className={`CreateList ${ this.state.addingList ? 'CreateList-adding-list' : '' }`}>
+        {
+          this.state.addingList ?
+            <div>
+              <input className="CreateList-input" type="text" name="name" placeholder="Add a list…" autoComplete="off"/>
+              <div className="CreateList-controls">
+                <input className="CreateList-button" type="submit" value="Save"/>
+                <a className="CreateList-close" href="#" onClick={this.toggleAddList}></a>
+              </div>
+            </div>
+          :
+            <span className="CreateList-placeholder" onClick={this.toggleAddList}>Add a list…</span>
+        }
       </div>
     );
   }
