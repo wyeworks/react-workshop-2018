c2d7b0fab568edb23944dc15eb1a7fb4b9b5e472
Emilio Cristalli <emilio@wyeworks.com>
2018-04-08 21:49:58 -0300

    Load lists from API

--- frontend/src/components/BoardsShow/BoardsShow.js
+++ frontend/src/components/BoardsShow/BoardsShow.js
@@ -4,20 +4,38 @@ import List from '../List/List';
 import CreateList from '../CreateList/CreateList';
 
 class BoardsShow extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", lists: [] };
+    this.boardId = this.props.match.params.bordId;
+  }
+
+  componentDidMount() {
+    fetch(`/boards/${this.boardId}`).then(res => res.json()).then((board) => {
+      this.setState({
+        name: board.name,
+        lists: board.lists
+      });
+    });
+  }
+
   render() {
     return (
       <div className="BoardsShow">
         <div className="BoardsShow-content">
           <div className="BoardsShow-header">
             <span className="BoardsShow-header-btn" href="#">
-              Board Name
+              {this.state.name}
             </span>
           </div>
 
           <div className="BoardsShow-canvas">
             <div className="BoardsShow-canvas-content">
-              <List name="List1" />
-              <List name="List2" />
+              {
+                this.state.lists.map((list) => {
+                  return <List name={list.name} key={list.id} />
+                })
+              }
 
               <CreateList />
             </div>
