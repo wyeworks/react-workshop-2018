36f195fc948a6c1e70be84495bfa383fb7fb6d33
Leticia Errandonea <errandonealeticia@gmail.com>
2018-05-08 11:27:47 -0300

    Mock fetch and fix test

---
+++ frontend/setup.js
@@ -0,0 +1,13 @@
+// Enzyme
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+global.shallow = shallow;
+
+// Fetch mock
+const boardsFetch = Promise.resolve({
+  json: () => Promise.resolve([{ id: 1, name: 'Board1' }]),
+});
+
+global.fetch = () => boardsFetch;
