208a64e94915a89b3e832cc51079759b76fefe4c
Diego Acosta <diego@wyeworks.com>
2018-05-28 11:18:22 -0300

    Navegacion

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
 import Boards from './components/Boards';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App-container">
-        <div className="App-header">
-          <a className="App-header-logo">
-            <span className="App-header-logo-default"></span>
-          </a>
+      <BrowserRouter>
+        <div className="App-container">
+          <div className="App-header">
+            <Link className="App-header-logo" to="/">
+              <span className="App-header-logo-default"></span>
+            </Link>
+          </div>
+          <div className="App-content">
+            <Route exact path='/' component={Boards}/>
+          </div>
         </div>
-        <div className="App-content">
-          <Boards/>
-        </div>
-      </div>
+      </BrowserRouter>
     );
   }
 }
